f66098d12e00049462da985a67a0ec654a73ebbf
USMK12 <111769057+USMK12@users.noreply.github.com>
2022-12-14 20:01:31 +0530

    Update pascaltriangle.java

--- pascaltriangle.java
+++ pascaltriangle.java
@@ -1,38 +1,28 @@
-import java.util.*
- public static void main(String[] args){
- List<Integer> li=new ArrayList<>();
- List<Integer> li1=new ArrayList<>();
- Scanner a=new Scanner(System.in);
- int c=1;
- int n=a.nextInt();
- li.add(1);
-
- li.add(1);
- for(int i=0;i<n;i++){
- for(int j=0;j<n-i;j++){
- System.out.print(" ");
- }
-
- if(c==1){
- System.out.print("1");
- }
- else{
- li1.add(1);
- for(int k=0;k<li.size();k++){
-
- System.out.print( li.get(k)+" ");
- if(k<li.size()-1){
-
- li1.add(li.get(k)+li.get(k+1));
- }
- }
- li1.add(1);
- li.clear();
- li.addAll(li1);
- li1.clear();
- }
- c++;
- System.out.println ();
- }
-}
+import java.io.*;
+ 
+class GFG {
+    public int factorial(int i)
+    {
+        if (i == 0)
+            return 1;
+        return i * factorial(i - 1);
+    }
+    public static void main(String[] args)
+    {
+        int n = 4, i, j;
+        GFG g = new GFG();
+        for (i = 0; i <= n; i++) {
+            for (j = 0; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(
+                    " "
+                    + g.factorial(i)
+                          / (g.factorial(i - j)
+                             * g.factorial(j)));
+            }
+            System.out.println();
+        }
+    }
 }
